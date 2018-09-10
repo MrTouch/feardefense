5e22b1797d030817fb807b9bdd5a2a35efb01b13
Aytac <aytac_07@msn.om>
2018-09-10 18:11:56 +0200

    Controller Controls

---
+++ Assets/Scripts/TowerModul.cs
@@ -0,0 +1,29 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerModul : MonoBehaviour {
+
+    public SteamVR_TrackedController right;
+    public SteamVR_TrackedController left;
+
+    private bool isTriggered;
+
+	// Use this for initialization
+	void Start () {
+        isTriggered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerStay(Collider other) {
+        if (other.gameObject.CompareTag("GameController")){
+            this.transform.SetParent(other.transform);
+            Debug.Log("touched");
+        }
+
+    }
+    }
