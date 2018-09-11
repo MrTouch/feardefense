f5976f9c67d023e286240299371c38deccc2815b
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-09-12 00:38:09 +0200

    connect and scale towers

---
+++ Assets/Scripts/ConnectTower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+using UnityEngine;
+
+public class ConnectTower : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += Handle_ObjectSnappedToDropZone; 
+    }
+
+    void Handle_ObjectSnappedToDropZone(object sender, SnapDropZoneEventArgs e)
+    {
+        //this.gameObject.AddComponent<FixedJoint>();
+        Transform tower = gameObject.transform.parent;
+        Debug.Log(tower.transform.localScale);
+        e.snappedObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        Debug.Log(e.snappedObject.transform.localScale);
+        //Destroy(e.snappedObject.GetComponent<Rigidbody>());
+        e.snappedObject.GetComponent<Collider>().enabled = false;
+        //this.gameObject.transform.localScale.Scale(new Vector3(0.3f , 0.3f, 0.3f));
+        Debug.Log("added Fixed Joint");
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	} 
+
+
+
+}
