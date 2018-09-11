bd0108bccc8e5f6c568d5c2cbf2d681136e06248
Aytac <aytac_07@msn.om>
2018-09-11 12:41:24 +0200

    zusammengeführt in table view 1

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,11 +6,15 @@ public class Controller : MonoBehaviour {
 
     private SteamVR_TrackedController controller;
     private SteamVR_Controller.Device device;
+    private Rigidbody rb;
+    private bool firstHaptic;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         controller = this.GetComponent<SteamVR_TrackedController>();
-	}
+        device = SteamVR_Controller.Input((int)controller.controllerIndex);
+        firstHaptic = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,21 +23,30 @@ public class Controller : MonoBehaviour {
 
     public void OnTriggerStay(Collider other)
     {
-
-        Rigidbody rb = other.GetComponent<Rigidbody>();
-        if (other.gameObject.CompareTag("Tower") && controller.triggerPressed)
-        {
-            device = SteamVR_Controller.Input((int)controller.controllerIndex);
-            device.TriggerHapticPulse(500);
-            other.transform.SetParent(this.transform);
-            rb.isKinematic = true;
-            Debug.Log("touched");
+        if (other.gameObject.CompareTag("Tower")) {
+            rb = other.GetComponent<Rigidbody>();
+            if (controller.triggerPressed)
+            {
+                triggerHaptic();
+                other.transform.SetParent(this.transform);
+                rb.isKinematic = true;
+                Debug.Log("touched");
+            }
+            else if(!controller.triggerPressed)
+            {
+                other.transform.parent = null;
+                rb.isKinematic = false;
+                firstHaptic = false;
+            }
         }
-        else if(!controller.triggerPressed)
-        {
-            other.transform.parent = null;
-            rb.isKinematic = false;
-        }
+    }
 
+    private void triggerHaptic()
+    {
+        if(!firstHaptic)
+        {
+            device.TriggerHapticPulse(3000);
+            firstHaptic = true;
+        }
     }
 }
