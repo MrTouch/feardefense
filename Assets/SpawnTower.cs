1c7e3593f15d3db20579b39e73fbbd87cc0f939e
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-09-12 18:15:52 +0200

    scripts and spawn

---
+++ Assets/SpawnTower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SpawnTower : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<VRTK_SnapDropZone>().ObjectUnsnappedFromDropZone += Handle_ObjectUnsnappedFromDropZone;
+    }
+
+    void Handle_ObjectUnsnappedFromDropZone(object sender, SnapDropZoneEventArgs e)
+    {
+        e.snappedObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
