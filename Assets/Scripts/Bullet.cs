4dc5a28d5f42c53aa180a4ce62f30210aefea718
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-09-12 16:58:38 +0200

    tower shoots

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 1f;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = target.position - this.transform.localPosition;
+
+        float distThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distThisFrame)
+        {
+            DoBulletHit();
+        }
+        else
+        {
+            //move towards node 
+            transform.Translate(dir.normalized * distThisFrame, Space.World);
+            Quaternion targetRotaton = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotaton, Time.deltaTime * 5);
+        }
+    }
+
+    void DoBulletHit()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    Transform turretTransform;
+
+    float range = 1f;
+
+    public GameObject bulletPrefab;
+
+    public float fireCooldown = 0.8f;
+    float fireCooldownLeft = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        turretTransform = transform.Find("Turret");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Enemy[] enemies = GameObject.FindObjectsOfType<Enemy>();
+
+        Enemy nearestEnemy = null;
+        float dist = Mathf.Infinity;
+
+        foreach(Enemy e in enemies)
+        {
+            float d = Vector3.Distance(this.transform.position, e.transform.position);
+            if(nearestEnemy == null || d < dist)
+            {
+                nearestEnemy = e;
+                dist = d;
+                Debug.Log("enemy found");
+                Debug.Log(e);
+            }
+
+            if(nearestEnemy == null)
+            {
+                Debug.Log("noenemies?");
+            }
+
+            Vector3 dir = nearestEnemy.transform.position - this.transform.position;
+            Quaternion lookRot = Quaternion.LookRotation(dir);
+
+            //rotate Tower to enemy direction
+            //turretTransform.rotation = Quaternion.Euler(0, lookRot.eulerAngles.y, 0);
+
+            fireCooldownLeft -= Time.deltaTime;
+            Debug.Log("firecooldownleft: " + fireCooldownLeft);
+            if (fireCooldownLeft <= 0)
+            {
+                fireCooldownLeft = fireCooldown;
+                ShootAt(nearestEnemy);
+            }
+        }
+	}
+    void ShootAt(Enemy e)
+    {
+        Debug.Log("shoot");
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, this.transform.position, this.transform.rotation);
+        Bullet b = bulletGo.GetComponent<Bullet>();
+        b.target = e.transform;
+    }
+}
