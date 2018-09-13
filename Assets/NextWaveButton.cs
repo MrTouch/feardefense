f0bc53304d259e68d79943e88853017ca3fa70d3
Aytac <aytac_07@msn.om>
2018-09-13 15:39:12 +0200

    next wave button

--- Assets/NextWaveButton.cs
+++ Assets/NextWaveButton.cs
@@ -6,11 +6,34 @@ public class NextWaveButton : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(DelayButton());
+    }
+
+    private IEnumerator DelayButton()
+    {
+        float yValue = transform.position.y;
+        while(yValue > 0.575f)
+        {
+            yValue -= 0.005f;
+            transform.position = new Vector3(transform.position.x, yValue, transform.position.z);
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return new WaitForSeconds(0.3f);
+
+        while(yValue < 0.6)
+        {
+            yValue += 0.005f;
+            transform.position = new Vector3(transform.position.x, yValue, transform.position.z);
+            yield return new WaitForSeconds(0.04f);
+        }
+
+    }
 }
