a3a5be816311573f54ff950e9dc069f2748b7cfe
Aytac <aytac_07@msn.om>
2018-09-14 11:02:52 +0200

    powerball iceball

---
+++ Assets/Scripts/Controller_Listener.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK; 
+using UnityEngine;
+
+public class Controller_Listener : MonoBehaviour {
+
+    public GameObject PowerBallPrefab;
+    private GameObject powerball;
+    private bool powerballIsLoading;
+	// Use this for initialization
+	void Start () {
+        GetComponent<VRTK_InteractGrab>().GrabButtonPressed += Controller_Listener_GrabButtonPressed;
+        GetComponent<VRTK_InteractGrab>().GrabButtonReleased += Controller_Listener_GrabButtonReleased;
+    }
+
+    private void Controller_Listener_GrabButtonReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("grip unclicked");
+        powerballIsLoading = false;
+        shootPowerBall(powerball, e);
+    }
+
+    private void Controller_Listener_GrabButtonPressed(object sender, ControllerInteractionEventArgs e)
+    {
+
+        Debug.Log("grip clicked");
+        powerball = Instantiate(PowerBallPrefab, e.controllerReference.model.transform.position, e.controllerReference.model.transform.rotation);
+        powerballIsLoading = true;
+        StartCoroutine(loadPowerBall(powerball));
+    }
+
+   
+
+
+    // Update is called once per frame
+    void Update () {
+	}
+
+    private IEnumerator loadPowerBall(GameObject PowerBall)
+    {
+       
+        while (powerballIsLoading == true)
+        {
+            PowerBall.transform.localScale += new Vector3(0.005f, 0.005f, 0.005f);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+
+    private void shootPowerBall(GameObject PowerBall, ControllerInteractionEventArgs e)
+    {
+        //shoot powerball
+        Debug.Log("shoot");
+    }
+
+}
---
+++ Assets/Scripts/magic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+using UnityEngine;
+
+public class magic : MonoBehaviour {
+
+    public GameObject powerballPrefab;
+
+    public bool powerballIsLoading = true;
+
+	// Use this for initialization
+	void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(GetComponent<VRTK_ControllerEvents>().triggerClicked);
+
+        GameObject PowerBall = Instantiate(powerballPrefab, other.transform.position, other.transform.rotation);
+        powerballIsLoading = true;
+        StartCoroutine(loadPowerBall(PowerBall));
+
+        Debug.Log("magic");
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        powerballIsLoading = false;
+        Debug.Log("release magic");
+
+    }
+
+    private IEnumerator loadPowerBall(GameObject PowerBall)
+    {
+
+        while (powerballIsLoading == true)
+        {
+            PowerBall.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+}
